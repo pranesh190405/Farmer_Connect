c7f656a25ea0597ceb00aa86ea70289dcf0c750e
Adithyaa Seyyone <152270532+adii11001@users.noreply.github.com>
2026-03-11 00:59:09 +0530

    Update next.config.mjs (#85)

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://agronexus-env.eba-zhws2hp8.ap-south-1.elasticbeanstalk.com//api/:path*',
+        destination: 'http://agronexus-env.eba-zhws2hp8.ap-south-1.elasticbeanstalk.com/api/:path*',
       },
     ];
   },
