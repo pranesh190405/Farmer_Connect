e6f4e4ee444b46694859e5b56a9d27d935cefe47
Bala Praneeth.S <praneethsundaram1930@gmail.com>
2026-03-10 10:08:56 +0530

    Integrated frontend changes by merging

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5002/api/:path*',
+        destination: 'http://localhost:5001/api/:path*',
       },
     ];
   },
