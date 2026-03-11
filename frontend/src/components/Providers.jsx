c529aea472232c9dc039d3e2cb545a7cd2bafa00
pranesh190405 <praneshcollege1904@gmail.com>
2026-03-11 09:55:45 +0530

    revert: remove AuthRestorer from Providers for now

--- frontend/src/components/Providers.jsx
+++ frontend/src/components/Providers.jsx
@@ -1,26 +1,13 @@
 'use client';
 
-import { useEffect } from 'react';
-import { Provider, useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
 import { store } from '@/store/store';
-import { fetchMe } from '@/store/slices/authSlice';
 import '@/lib/i18n';
-
-// Restores auth state from server session cookie after page reload
-function AuthRestorer({ children }) {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchMe());
-    }, [dispatch]);
-    return children;
-}
 
 export default function Providers({ children }) {
     return (
         <Provider store={store}>
-            <AuthRestorer>
-                {children}
-            </AuthRestorer>
+            {children}
         </Provider>
     );
 }
